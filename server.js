9277c1cc2a4828cc82a790d761d0ea5aedc76849
Sami Byers <samtaylor.byers@gmail.com>
2021-11-19 13:59:49 -0600

    done

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
+require('dotenv').config();
 
 const PORT = process.env.PORT || 3000
 
@@ -19,7 +20,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false,
   useCreateIndex: true,
-  useFindAndModify: false
+  useUnifiedTopology: true
 });
 
 // routes
